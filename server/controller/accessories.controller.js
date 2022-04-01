fff12ffc0574ab5b74255c2bf823980b3185b330
Y4ROSLAV <jeros1337@gmail.com>
2022-04-01 21:41:36 +0300

    change id field and add an url property

--- server/controller/accessories.controller.js
+++ server/controller/accessories.controller.js
@@ -2,7 +2,7 @@ const db = require('../db')
 
 class AccesoriesController {
   async getAccesories(req, res) {
-    const accessories = await db.query(`SELECT subtype_id as id, title, picture FROM subtype 
+    const accessories = await db.query(`SELECT subtype_id as id, title, picture, '/accessories/' || subtype_id as url FROM subtype
     WHERE fk_type_id = (SELECT type_id FROM type WHERE title = 'Аксессуары')`)
 
     res.json(accessories.rows)
--- server/controller/clothes.controlles.js
+++ server/controller/clothes.controlles.js
@@ -2,8 +2,7 @@ const db = require('../db')
 
 class ClothesController {
   async getClothes(req, res) {
-    const clothes = await db.query(`SELECT subtype_id as id, title, picture FROM subtype 
-    WHERE fk_type_id = (SELECT type_id FROM type WHERE title = 'Одежда')`)
+    const clothes = await db.query(`SELECT subtype_id as id, title, picture, '/clothes/' || subtype_id as url FROM subtype WHERE fk_type_id = (SELECT type_id FROM type WHERE title = 'Одежда')`)
 
     res.json(clothes.rows)
   }
